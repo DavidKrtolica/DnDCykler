700e465b552c6fcabdfd7609287b52f1b3620248
David <davidharingri@gmail.com>
2020-10-20 19:24:48 +0200

    Fixed small issue with package importing.

--- src/main/java/com/example/demo/repository/BikeOrderRepository.java
+++ src/main/java/com/example/demo/repository/BikeOrderRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.BikeOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BikeOrderRepository extends JpaRepository<BikeOrder, Integer> {
--- src/main/java/com/example/demo/repository/BikeRepository.java
+++ src/main/java/com/example/demo/repository/BikeRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.Bike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BikeRepository extends JpaRepository<Bike, Integer> {

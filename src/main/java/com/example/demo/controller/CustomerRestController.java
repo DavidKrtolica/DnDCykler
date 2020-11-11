10933ccf79acd266868e00a080e11d1b47bd5ceb
David <david.krtolica8@gmail.com>
2020-11-11 17:12:41 +0100

    Implementation of service package. Finished customer service class and refactored customer controller

--- src/main/java/com/example/demo/controller/CustomerRestController.java
+++ src/main/java/com/example/demo/controller/CustomerRestController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Customer;
 import com.example.demo.repository.CustomerRepository;
+import com.example.demo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -21,18 +22,8 @@ import java.util.Map;
 public class CustomerRestController {
 
     @Autowired
-    CustomerRepository customerRepository;
+    CustomerService customerService;
 
-
-    //METHOD RETRIEVING THE SORT.DIRECTON ENUM
-    private Sort.Direction getSortDirection(String direction){
-        if (direction.equals("asc")) {
-            return Sort.Direction.ASC;
-        } else if (direction.equals("desc")) {
-            return Sort.Direction.DESC;
-        }
-        return Sort.Direction.ASC;
-    }
 
     //GET MAPPING WITH PAGINATION, FILTERING & SORTING TOGETHER - FINAL VERSION
     @GetMapping("/customers")
@@ -41,101 +32,42 @@ public class CustomerRestController {
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "4") int size,
             @RequestParam(defaultValue = "customerId,desc") String[] sort){
-
-        List<Sort.Order> orders = new ArrayList<>();
-        if(sort[0].contains(",")){
-            //WILL SORT MORE THAN 2 FIELDS, SORTORDER = "FIELD, DIRECTION"
-            for (String sortOrder : sort){
-                String[] _sort = sortOrder.split(",");
-                orders.add(new Sort.Order(getSortDirection(_sort[1]),_sort[0]));
-            }
-        } else {
-            //SORT = [FIELD, DIRECTION]
-            orders.add(new Sort.Order(getSortDirection(sort[1]), sort[0]));
-        }
-
-        Pageable paging = PageRequest.of(page, size, Sort.by(orders));
-
-        Page<Customer> pageCustomers;
-        if (firstName == null){
-            pageCustomers = customerRepository.findAll(paging);
-        } else {
-            pageCustomers = customerRepository.findByFirstNameContaining(firstName, paging);
-        }
-        List<Customer> customers = pageCustomers.getContent();
-        if (customers.isEmpty()) {
-            throw new ResourceNotFoundException("There are no customers!");
-        }
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("customers", customers);
-        response.put("currentPage", pageCustomers.getNumber());
-        response.put("totalItems", pageCustomers.getTotalElements());
-        response.put("totalPages", pageCustomers.getTotalPages());
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return customerService.getAllCustomers(firstName, size, sort, page);
     }
 
 
     //GET MAPPING FOR FINDING A CUSTOMER BY ID
     @GetMapping("/customers/{id}")
     public ResponseEntity<Customer> getCustomerById(@PathVariable("id") int id) {
-
-        Customer customer = customerRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Cannot find customer with ID = " + id));
-        return new ResponseEntity<>(customer, HttpStatus.OK);
+        return customerService.getCustomerByCustomerId(id);
     }
 
 
     //POST MAPPING FOR ADDING NEW CUSTOMER
     @PostMapping("/customers")
     public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
-
-            Customer newCustomer = customerRepository.save(customer);
-            return new ResponseEntity<>(newCustomer, HttpStatus.CREATED);
+        return customerService.createNewCustomer(customer);
     }
 
 
     //PUT MAPPING FOR UPDATING CUSTOMERS BY ID
     @PutMapping("/customers/{id}")
     public ResponseEntity<Customer> updateCustomerById(@PathVariable("id") int id, @RequestBody Customer updatedCustomer){
-
-            Customer customer = customerRepository.findById(id).
-                    orElseThrow(() -> new ResourceNotFoundException("Cannot find customer with ID = " + id));
-
-            customer.setFirstName(updatedCustomer.getFirstName());
-            customer.setLastName(updatedCustomer.getLastName());
-            customer.setAddress(updatedCustomer.getAddress());
-            customer.setPhoneNr(updatedCustomer.getPhoneNr());
-            customer.setEmail(updatedCustomer.getEmail());
-
-            final Customer customerFinal = customerRepository.save(customer);
-            return new ResponseEntity<>(customerFinal, HttpStatus.OK);
+        return customerService.updateCustomer(id, updatedCustomer);
     }
 
 
     //DELETE MAPPING FOR DELETING ALL CUSTOMERS
     @DeleteMapping("/customers")
     public ResponseEntity<Customer> deleteAllCustomers(){
-
-            List<Customer> customers = customerRepository.findAll();
-            if (customers.isEmpty()){
-                throw new ResourceNotFoundException("There are no customers!");
-            } else {
-                customerRepository.deleteAll();
-                return new ResponseEntity<>(HttpStatus.OK);
-            }
+        return customerService.deleteCustomers();
     }
 
 
     //DELETE MAPPING FOR DELETING A CUSTOMER BY ITS ID
     @DeleteMapping("/customers/{id}")
     public ResponseEntity<Customer> deleteCustomerById(@PathVariable("id") int id){
-
-            Customer customer = customerRepository.findById(id).
-                    orElseThrow(() -> new ResourceNotFoundException("Cannot find customer with ID = " + id));
-            customerRepository.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.OK);
+        return customerService.deleteCustomerByCustomerId(id);
     }
 
 }

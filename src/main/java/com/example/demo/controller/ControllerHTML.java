9d2d148cd7bd1047ba126b2f8d34b4ab589bdb0c
David <david.krtolica8@gmail.com>
2020-12-14 13:42:40 +0100

    Fixing HTML controller for deployment url

--- src/main/java/com/example/demo/controller/ControllerHTML.java
+++ src/main/java/com/example/demo/controller/ControllerHTML.java
@@ -5,19 +5,17 @@ import com.example.demo.repository.BikeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@CrossOrigin(origins = {"http://localhost:8081", "https://dnd-cykler-frontend.herokuapp.com"})
 @Controller
 public class ControllerHTML {
 
     @Autowired
     BikeRepository bikeRepository;
 
-    @GetMapping("/")
+    @GetMapping("/view")
     public String index(Model model) {
         List<Bike> bikes = bikeRepository.findAll();
         model.addAttribute("bikes", bikes);
